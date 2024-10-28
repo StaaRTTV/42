f917561715e5ec22d477a951da89c0a0c51c4cdc
Gabrielle Pochon <gpochon@c1r8s5.42luxembourg.lu>
2024-10-28 12:06:36 +0100

    fixed trim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int	ft_strlen(const char *str)
 {
@@ -55,23 +57,31 @@ char	*ft_strchr(const char *str, int c)
 	return (NULL);
 }
 
-char	*ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(const char  *s1, const char  *set)
 {
 	size_t	start;
 	size_t	end;
 	char	*str;
-
+	
+	if (!s1 || !set)
+		return (NULL);
 	start = 0;
 	end = ft_strlen(s1);
-	if (s1 && set)
-	{
-		while (s1[start] && ft_strchr(set, s1[start]))
-			start++;
-		while (s1[end - 1] && ft_strchr(set, s1[end - 1] && end > start))
-			end--;
-		str = malloc(sizeof(char) * (end - start + 1));
-		if (str)
-			ft_strlcpy(str, (char *)(s1 + start), end - start + 1);
-	}
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	str = malloc(sizeof(char) * (end - start + 1));
+	if (str)
+		ft_strlcpy(str, (char *)(s1 + start), end - start + 1);
 	return (str);
 }
+
+/*int	main()
+{
+	const char	str[] = "ooooo[jesuisfionnnnbeau]oooo";
+	const char	rjer[] = "o";
+	
+	printf("%s", ft_strtrim(str, rjer));
+	return (0);
+}*/
