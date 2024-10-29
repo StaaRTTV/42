ed8c60aecf986b14e6fc9273784dd0990048fe43
Gabrielle Pochon <gpochon@c1r9s4.42luxembourg.lu>
2024-10-29 15:08:23 +0100

    added strmapi (fonctionne ap)

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpochon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 14:56:07 by gpochon           #+#    #+#             */
+/*   Updated: 2024/10/29 14:56:20 by gpochon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include "libft.h"
 
-char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    unsigned int    index;
-    char    *result;
-    
-    index = 0;
-    result = malloc(ft_strlen(s) * sizeof(char));
-    while (s[index])
-    {
-        result[index] = f(index, s[index]);
-        index++;
-    }
-    return (result);
+	unsigned int	index;
+	char			*result;
+
+	index = 0;
+	result = malloc(ft_strlen(s) * sizeof(char));
+	while (s[index])
+	{
+		result[index] = f(index, s[index]);
+		index++;
+	}
+	return (result);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -32,6 +32,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_itoa(int n);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
