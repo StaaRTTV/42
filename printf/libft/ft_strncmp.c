88906d8dd2b89a4416f36a5439036ce72710b583
Gabrielle Pochon <gpochon@c1r9s4.42luxembourg.lu>
2024-10-30 16:48:24 +0100

    JE COMMENCE PRINTF FRERO

---
+++ printf/libft/ft_strncmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpochon <gpochon@student.42luxembourg.l    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 21:21:14 by gpochon           #+#    #+#             */
+/*   Updated: 2024/10/06 21:41:34 by gpochon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (len > i && s1[i] && s1[i] == s2[i])
+		i++;
+	if (i == len)
+		return (0);
+	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+}
---
+++ printf/libft/ft_substr.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpochon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 09:52:27 by gpochon           #+#    #+#             */
+/*   Updated: 2024/10/28 09:56:54 by gpochon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t		i;
+	char		*result;
+	size_t		slen;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (start >= slen)
+		return (ft_strdup(""));
+	if (len > slen - start)
+		len = slen - start;
+	result = malloc((len + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	while (s[start] && i < len)
+	{
+		result[i] = s[start];
+		i++;
+		start++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+/*
+int	main(void)
+{
+	char	test[] = "wesh les amigos";
+
+	printf("%s", ft_substr(test, 5, 5));
+	return (0);
+}
+*/
