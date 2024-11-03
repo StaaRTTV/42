76c72b228e81994d7250553ddc6a9882de8de70a
Gabz <staarpublic@gmail.com>
2024-11-03 19:15:50 +0100

    a recup a 42

--- common-core/printf/ft_printf.c
+++ common-core/printf/ft_printf.c
@@ -6,25 +6,28 @@
 /*   By: gpochon <gpochon@student.42luxembourg.l    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 15:29:53 by gpochon           #+#    #+#             */
-/*   Updated: 2024/11/03 18:08:17 by gpochon          ###   ########.fr       */
+/*   Updated: 2024/11/03 19:15:33 by gpochon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
-static void	ft_percent_what(char c)
+static void	ft_percent_what(char c, va_list args)
 {
 	if (c == 'c')
 		ft_printchar(c);
 	else if (c == 's')
 		ft_printstring(c);
 	else if (c == 'p')
+		ft_printchar(c); // a changer
 	else if (c == 'd' || c == 'i')
-		ft_printnb(c);
+		ft_printnbr(c); 
 	else if (c == 'u')
-		ft_printnb(c); // fonction a creer 
+		ft_printnbr(c); // fonction a creer 
 	else if (c == 'x')
+		ft_printchar(c); // a changer
 	else if (c == 'X')
+		ft_printchar(c); // a changer
 	else if (c == '%')
 		ft_printchar('%');
 }
@@ -44,7 +47,7 @@ int	ft_printf(const char *format, ...)
 		{
 			len++;
 			format++;
-			ft_percent_what(*format);
+			ft_percent_what(*format, args);
 		}
 		else
 		{
--- common-core/printf/ft_printf.h
+++ common-core/printf/ft_printf.h
@@ -6,9 +6,12 @@
 /*   By: gpochon <gpochon@student.42luxembourg.l    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 17:22:14 by gpochon           #+#    #+#             */
-/*   Updated: 2024/11/03 17:53:40 by gpochon          ###   ########.fr       */
+/*   Updated: 2024/11/03 18:18:36 by gpochon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 #include <stdarg.h>
 #include <unistd.h>
@@ -17,3 +20,5 @@ int	ft_printf(const char *format, ...);
 void	ft_printchar(const char c);
 void	ft_printstring(char *str);
 void	ft_printnbr(int nb);
+
+#endif
