087ce0457fa9c0c837bcaa29cf35f9093c10aa1a
Gabrielle Pochon <gpochon@c1r9s3.42luxembourg.lu>
2024-11-08 15:42:54 +0100

    get_next_line suite

--- common-core/get-next-line/get_next_line.c
+++ common-core/get-next-line/get_next_line.c
@@ -6,50 +6,68 @@
 /*   By: gpochon <gpochon@student.42luxembourg.l    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 19:21:24 by gpochon           #+#    #+#             */
-/*   Updated: 2024/11/07 19:38:34 by gpochon          ###   ########.fr       */
+/*   Updated: 2024/11/08 15:42:21 by gpochon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*fill_buffer(int fd, t_list *last_c, int alr_read)
+static char *read_and_fill(int fd, char *remainder, int *bytes_read)
 {
-	char	*buffer;
+    char *buffer;
+    char *temp;
 
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-		return ;
-	while (ft_strchr(last_c, '\n'))
-	{
-		*alr_read = read(fd, buffer, BUFFER_SIZE);
-		if (*alr_read <= 0)
-			break ;
-		buffer[alr_read] = NULL;
-	}
-	return (*last_c);
+    buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!buffer)
+        return (NULL);
+    while (!ft_strchr(*remainder, '\n'))
+    {
+        *bytes_read = read(fd, buffer, BUFFER_SIZE);
+        if (*bytes_read <= 0)
+            break;
+        buffer[*bytes_read] = '\0';
+        temp = remainder;
+        remainder = ft_strjoin(remainder, buffer);
+        free(temp);
+        if (!remainder)
+        {
+            free(buffer);
+            return (NULL);
+        }
+    }
+    free(buffer);
+    return (*remainder);
 }
 
-static char	*set_line(char *line_buff)
+static char *extract_line(char *remainder)
 {
+    char *line;
+    char *new_remainder;
+    int len;
+
+    len = 0;
+    while (remainder[len] && remainder[len] != '\n')
+    {
+        line = ft_strdup(remainder);
+    }
 }
 
-char	*get_next_line(int fd)
+char *get_next_line(int fd)
 {
-	int				alr_read;
-	char			*line;
-	static t_list	*last_c;
+    int bytes_read;
+    char *line;
+    static char *remainder;
 
-	last_c = NULL;
-	line = NULL;
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
-	if (read(fd, 0, 0) < 0)
-		return (-1);
-	alr_read = 0;
-	last_c = fill_buffer(fd, &last_c, &alr_read);
-	if (last_c)
-		line = set_line(last_c);
-	return (line);
+    remainder = NULL;
+    line = NULL;
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (0);
+    bytes_read = 0;
+    remainder = read_and_fill(fd, remainder, &bytes_read);
+    if (!remainder)
+        return (NULL);
+    line = extract_line(&remainder);
+    return (line);
 }
 /*
 #include <stdio.h>
