515606ed099802cd92f8e1977a358459c9be7e9e
Gabrielle Pochon <gpochon@c1r9s4.42luxembourg.lu>
2025-01-14 10:41:49 +0100

    Adding some operation

--- common-core/push_swap/includes/push_swap.h
+++ common-core/push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: gpochon <gpochon@student.42luxembourg.l    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:59:14 by gpochon           #+#    #+#             */
-/*   Updated: 2025/01/13 12:26:54 by gpochon          ###   ########.fr       */
+/*   Updated: 2025/01/14 10:04:45 by gpochon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,10 @@ typedef enum e_stack_id
 	STACK_A,
 	STACK_B
 } t_stack_id;
+
+void	rot(t_stack *stacks, t_stack_id which);
+void	rrot(t_stack *stacks, t_stack_id which);
+void	rr(t_stack *stacks);
 void	ft_error(char *str);
 int		*tabatoi(char **table, int which, int *size_a);
 int		*init_stack_b(int *stack_a, int size_a);
--- common-core/push_swap/src/ps_ops.c
+++ common-core/push_swap/src/ps_ops.c
@@ -6,37 +6,76 @@
 /*   By: gpochon <gpochon@student.42luxembourg.l    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 11:18:48 by gpochon           #+#    #+#             */
-/*   Updated: 2025/01/13 16:04:57 by gpochon          ###   ########.fr       */
+/*   Updated: 2025/01/14 10:40:37 by gpochon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void *push(t_stack *stacks, t_stack_id dest)
+void push_a(t_stack *stacks)
 {
-	if (dest == STACK_A)
+	int i;
+
+	i = stacks->size_a;
+	if (stacks->size_b <= 0)
+		return ;
+	while (i > 0)
 	{
-		
+		stacks->stack_a[i] = stacks->stack_a[i - 1];
+		i--;
 	}
-	else if (dest == STACK_B)
+	stacks->stack_a[0] = stacks->stack_b[0];
+	i = 0;
+	while (i < stacks->size_b - 1)
 	{
-		
+		stacks->stack_b[i] = stacks->stack_b[i + 1];
+		i++;
+	}
+	stacks->size_a++;
+	stacks->size_b--;
+}
+void push_b(t_stack *stacks)
+{
+	int i;
+
+	i = stacks->size_b;
+	if (stacks->size_a >= 0)
+		return ;
+	while (i > 0)
+	{
+		stacks->stack_b[i] = stacks->stack_b[i - 1];
+		i--;
+	}
+	stacks->stack_b[0] = stacks->stack_a[0];
+	i = 0;
+	while (i < stacks->size_a - 1)
+	{
+		stacks->stack_a[i] = stacks->stack_a[i + 1];
+		i++;
+	}
+	stacks->size_b++;
+	stacks->size_a--;
+}
+
+void swap(t_stack *stacks, t_stack_id dest)
+{
+	int tmp;
+
+	if (dest == STACK_A && stacks->size_a > 1)
+	{
+		tmp = stacks->stack_a[0];
+		stacks->stack_a[0] = stacks->stack_a[1];
+		stacks->stack_a[1] = tmp;
+	}
+	else if (dest == STACK_B && stacks->size_b > 1)
+	{
+		tmp = stacks->stack_b[0];
+		stacks->stack_b[0] = stacks->stack_b[1];
+		stacks->stack_b[1] = tmp;
 	}
 }
 
-void *swap(t_stack *stacks, t_stack_id dest)
-{
-	if (dest == STACK_A)
-	{
-		
-	}
-	else if (dest == STACK_B)
-	{
-		
-	}
-}
-
-void *ss_swap(t_stack *stacks)
+void ss_swap(t_stack *stacks)
 {
 	swap(stacks, STACK_A);
 	swap(stacks, STACK_B);
